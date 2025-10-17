6a2f11b4b4d90a32c23765135cb42350c68313a4
carterjgreene360 <63014719+carterjgreene360@users.noreply.github.com>
2025-10-17 17:44:28 -0500

    11/70 flamethrower and trap fixes (#78)
    * Visual adjustments for certain traps
    
    * Flamethrower Visual

---
+++ Assets/Scripts/Soul Tower/Traps/UI/Trap UI/Flamethrower/FlamethrowerAnimation.cs
@@ -0,0 +1,42 @@
+using Shears;
+using Shears.Tweens;
+using SoulTower.Towers;
+using System.Collections;
+using UnityEngine;
+
+namespace SoulTower.Traps.UI
+{
+    public class FlamethrowerAnimation : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private FlamethrowerTrap flamethrower;
+        [SerializeField] private Transform nozzle;
+        [SerializeField] private ParticleSystem fire1;
+        [SerializeField] private ParticleSystem fire2;
+        [SerializeField] private ParticleSystem ember;
+
+        [SerializeField] private TweenData shakeTweenData;
+
+        private Tween tween;
+
+        private void OnEnable()
+        {
+            flamethrower.Activated += OnFlamethrowerActivated;
+        }
+
+        private void OnDisable()
+        {
+            flamethrower.Activated -= OnFlamethrowerActivated;
+        }
+
+        private void OnFlamethrowerActivated(Trap _)
+        {
+            fire1.Play();
+            fire2.Play();
+            ember.Play();
+
+            tween = nozzle.DoShakeTween(.05f, 0.1f, shakeTweenData);
+            tween.Completed += () => tween.Dispose();
+        }
+    }
+}
