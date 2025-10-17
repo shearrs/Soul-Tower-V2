20d0d9875a91cb853d18a5ebad6814836b59b568
reagancheatham <reagancheatham@gmail.com>
2025-10-17 03:34:46 -0500

    Fix hologram instantiation

--- Assets/Scripts/Soul Tower/Players/UI/TrapPlacementUI.cs
+++ Assets/Scripts/Soul Tower/Players/UI/TrapPlacementUI.cs
@@ -24,6 +24,7 @@ namespace SoulTower.Players.UI
 
         private void Awake()
         {
+            hologramMaterial = Instantiate(hologramMaterial);
             cancelInput = inputProvider.GetInput("Cancel Placement");
         }
 
