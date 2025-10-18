69f987361a7beca6edaf56382a8f785c5dc71a8e
reagancheatham <reagancheatham@gmail.com>
2025-10-18 00:42:19 -0500

    Increase max trap size

--- Assets/Scripts/Soul Tower/Traps/TrapData.cs
+++ Assets/Scripts/Soul Tower/Traps/TrapData.cs
@@ -13,7 +13,7 @@ namespace SoulTower.Traps
         [SerializeField, ShowIf("!isPassive")] private float cooldown = 5f;
 
         [Header("Placement")]
-        [SerializeField, Range(1, 3)] private int size = 1;
+        [SerializeField, Range(1, 4)] private int size = 1;
         [SerializeField] private TileType placementType = TileType.Floor;
 
         [Header("Damage")]
