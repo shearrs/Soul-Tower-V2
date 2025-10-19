ec1e72c661a500f5a3044aeea08373dfaf58c1b3
carterjgreene360 <carterjgreene360@gmail.com>
2025-10-19 02:24:45 -0500

    Mouse Over and Multiple Trap Damage Enablings
    - Dart Trap
    - Flamethrower
    - Started Freeze Breeze (blocked by bug)

--- Assets/Scripts/Soul Tower/Traps/Types/FlamethrowerTrap.cs
+++ Assets/Scripts/Soul Tower/Traps/Types/FlamethrowerTrap.cs
@@ -8,6 +8,38 @@ namespace SoulTower.Traps
     [RequireComponent(typeof(Trap))]
     public class FlamethrowerTrap : ManagedWrapper<Trap>
     {
+        [SerializeField] private HitBody3D[] hitbodys;
+
+        private Timer activeTimer = new Timer(2.5f);
+
         public event Action<Trap> Activated { add => TypedWrappedValue.Activated += value; remove => TypedWrappedValue.Activated -= value; }
+
+        private void OnEnable()
+        {
+            Activated += StartFlames;
+            activeTimer.Completed += EndFlames;
+        }
+        private void OnDisable()
+        {
+            Activated -= StartFlames;
+            activeTimer.Completed -= EndFlames;
+        }
+
+        private void StartFlames(Trap _)
+        {
+            foreach (var box in hitbodys)
+            {
+                box.enabled = true;
+            }
+
+            activeTimer.Start();
+        }
+        private void EndFlames()
+        {
+            foreach (var box in hitbodys)
+            {
+                box.enabled = false;
+            }
+        }
     }
 }
--- Assets/Scripts/Soul Tower/Traps/Types/FreezeBreeze.cs
+++ Assets/Scripts/Soul Tower/Traps/Types/FreezeBreeze.cs
@@ -8,6 +8,29 @@ namespace SoulTower.Traps
     [RequireComponent(typeof(Trap))]
     public class FreezeBreeze : ManagedWrapper<Trap>
     {
+        [SerializeField] private HitBody3D hitbody;
+        private Timer activeTimer = new Timer(2f);
+
         public event Action<Trap> Activated { add => TypedWrappedValue.Activated += value; remove => TypedWrappedValue.Activated -= value; }
+        private void OnEnable()
+        {
+            Activated += StartFrost;
+            activeTimer.Completed += EndFrost;
+        }
+        private void OnDisable()
+        {
+            Activated -= StartFrost;
+            activeTimer.Completed -= EndFrost;
+        }
+
+        private void StartFrost(Trap _)
+        {
+            hitbody.enabled = true;
+            activeTimer.Start();
+        }
+        private void EndFrost()
+        {
+            hitbody.enabled = false;
+        }
     }
 }
